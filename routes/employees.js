942be3c56fbf68287c1e0cd68a7edc03a145a044
Adrián Ramón <adrianramon2003@gmail.com>
2023-02-03 09:09:11 +0100

    Creados controladores de employees

--- routes/employees.js
+++ routes/employees.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const getEmployees=require("../controllers/employees.controller")
-const postEmployees=require("../controllers/employees.controller")
-const putEmployees=require("../controllers/employees.controller")
-const deleteEmployees=require("../controllers/employees.controller")
+
+const {getEmployees, postEmployees, putEmployees, deleteEmployees}=require("../controllers/employees.controller")
+
 
 
 router.get('/employees', getEmployees);
